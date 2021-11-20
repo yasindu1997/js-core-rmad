7e3f086b0b2d09c0137acd6f41d59e63a1c4265e
yasindu1997 <yasindusathsara@gmail.com>
2021-11-20 15:07:38 +0530

    initial commit

---
+++ main.js
@@ -0,0 +1,54 @@
+alert("Hello Yasindu !");
+
+console.log("Hello Yasindu !"); //js statement
+
+//console.log("Welcome to RMAD !");
+
+/*console.log("Hello");
+console.log("Hello");
+console.log("Hello");*/
+
+//variables
+
+var x = 10;
+let y = 20;
+const z = 30;
+
+var x = 20;
+let y = 40; //let cannot redeclare
+const z = 60; //const cannot redeaclare
+
+x = "yasindu";
+y = "RMAD";
+z = "IJSE"; //const variables cannot reassign values
+
+// js datatypes
+
+let age = 25; //number
+let address = "panadura, srilanka"; //String
+let isMarried = false; //boolean
+let job; //undefined
+let yasindu = { name: "yasindu", age: 25, address: "panadura" }
+let dell_inspiron_3576 = { modal: "dell-inspiron", modelNo: 3576, ram: '12gb', hdd: 512 } //object
+
+console.log(job);
+
+//operators
+
+//+,-,*,/,+=,++,-- (increment operator , decremental operator),%
+let sum = 5 + 4;
+
+let num1 = 10;
+let num2 = 3;
+
+let sum2 = num1 - num2;
+
+num1 += 10; //num1= num1 + 10;
+
+console.log(num1);
+
+num2--;
+
+console.log(num2);
+
+console.log(num1 % num2); //modular / reminder
